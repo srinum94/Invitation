238a28284336197b4c3c255b6104943b4472aa29
srinum94 <srinum94@users.noreply.github.com>
2026-05-15 16:27:49 +0000

    deploy: 8665609dfb099a0de3e422f778e0a1da9eb31aad

---
+++ assets/use-config-Boa0WDqb.js
@@ -0,0 +1,1 @@
+import{u as a,a as n}from"./index-QVgdA-rg.js";function o(){const{config:o}=a();return o||n.data}export{o as u};
--- assets/use-config-DLYnZ1uF.js
+++
@@ -1,1 +0,0 @@
-import{u as a,a as n}from"./index-CNnr-qxx.js";function o(){const{config:o}=a();return o||n.data}export{o as u};
