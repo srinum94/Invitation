9b224e63102c4d7e676e32d6ed7ddbf73afd82ac
srinum94 <srinum9989@gmail.com>
2026-05-15 19:15:57 +0530

    Fix base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "Invitation", //  ADD THIS LINE
+    base: "/Invitation/", // ✅ FIXED
 
     plugins: [react()],
     resolve: {
